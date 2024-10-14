01f139ce4946c397391c03c30563cfd601cb9e10
Holovko Maksym <147420858+MaxsimJSDeveloper@users.noreply.github.com>
2024-10-14 11:35:44 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="uk">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-control" content="no-transform" />
+    <meta http-equiv="Cache-control" content="no-cache" />
+    <meta name="google" content="nopagereadaloud" />
+    <link rel="icon" type="image/svg+xml" href="./assets/img/favicon.webp" />
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <title>Продажа дорогого апаратного массажного обладнання</title>
+
+    <meta name="description" content="Спеціалізуємося на продажу високоякісного апаратного масажного обладнання. Наші продукти допоможуть вам досягти релаксації та покращити загальне самопочуття." />
+    <meta name="keywords" content="апаратне масажне обладнання, продаж масажерів, високоякісне обладнання, релаксація, здоров'я, wellness, масаж" />
+    <meta name="robots" content="index, follow" />
+</head>
+
+<body>
+    <?php include './public/components/header.php' ?>
+    <main>
+        <?php include './public/components/main.php' ?>
+        <?php include './public/components/about.php' ?>
+        <?php include './public/components/products.php' ?>
+        <?php include './public/components/advantages.php' ?>
+        <?php include './public/components/contacts.php' ?>
+    </main>
+    <?php include './public/components/nav-modal.php' ?>
+    <?php include './public/components/cookie-notice.php' ?>
+    
+    <script src="./assets/js/cookieNotice.js" defer></script>
+    <script src="./assets/js/modal.js" defer></script>
+    <script src="./assets/js/formHandler.js" defer></script>
+</body>
+
+</html>
---
+++ public/components/advantages.php
@@ -0,0 +1,24 @@
+<section id="advantages" class="section-indentation" aria-labelledby="advantages-title"> 
+    <div class="container">
+        <h2 class="title" id="advantages-title">Переваги</h2>
+        <ul class="event-benefits">
+            <li class="benefit-item">
+                <h3 class="benefit-title">Надійність</h3>
+                <p class="benefit-description">Наше апаратне масажне обладнання створене з використанням сучасних технологій та міцних матеріалів, що забезпечує високу ефективність та довгий термін служби. Вкладайтесь у своє здоров'я з перевіреними рішеннями.</p>
+            </li>
+            <li class="benefit-item">
+                <h3 class="benefit-title">Підхід</h3>
+                <p class="benefit-description">Консультації, щоб допомогти вам вибрати обладнання відповідно до ваших потреб. Наша команда готова надати рекомендації та підтримку на всіх етапах покупки.</p>
+            </li>
+            <li class="benefit-item">
+                <h3 class="benefit-title">Підтримка</h3>
+                <p class="benefit-description">Надаємо довготривале обслуговування для всіх наших продуктів, щоб гарантувати безперебійну роботу обладнання. Ви можете бути впевнені у своєму виборі.</p>
+            </li>
+            <li class="benefit-item">
+                <h3 class="benefit-title">Комфорт</h3>
+                <p class="benefit-description">Наше обладнання допомагає зняти стрес, покращити кровообіг і відновити енергію. Вкладайтесь у своє здоров'я і самопочуття – це важливо для вас.</p>
+            </li>
+        </ul>
+    </div>
+</section>
+
